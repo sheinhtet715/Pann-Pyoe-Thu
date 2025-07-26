6eb61b655d2c938ca71477b2ec9284d16ad885dc
AyeSuMon_TinZaTial <tinzatial1610@gmail.com>
2025-07-26 22:33:56 +0630

    Backend part of courses (remain enrollment or payment )

---
+++ JavaScript/Courses.js
@@ -0,0 +1,12 @@
+// Profile‑menu toggle
+// function toggleProfileMenu() {
+//   const menu = document.getElementById("profile-menu");
+//   if (menu) menu.classList.toggle("show");
+// }
+// document.addEventListener('click', (e) => {
+//   const section = document.querySelector('.profile-section');
+//   const menu    = document.getElementById("profile-menu");
+//   if (section && menu && !section.contains(e.target)) {
+//     menu.classList.remove("show");
+//   }
+// });
---
+++ PHP/Controller/CoursesController.php
@@ -0,0 +1,51 @@
+<?php
+class CoursesController {
+    private $conn;
+    public function __construct(mysqli $conn) {
+        $this->conn = $conn;
+    }
+
+    /** @return array<int,array> */
+    public function getAllCourses(): array {
+        $sql = "SELECT * FROM Course_tbl WHERE is_upcoming = 0 ORDER BY course_name";
+        $res = $this->conn->query($sql);
+        return $res
+            ? $res->fetch_all(MYSQLI_ASSOC)
+            : [];
+    }
+}
+
+class PopularCourse {
+    private $conn;
+
+    public function __construct(mysqli $conn) {
+        $this->conn = $conn;
+    }
+
+    /** @return array<int,array> */
+    public function getPopularCourses(): array {
+        $sql = "SELECT * FROM Course_tbl WHERE is_popular = 1 ORDER BY course_name";
+        $res = $this->conn->query($sql);
+        return $res
+            ? $res->fetch_all(MYSQLI_ASSOC)
+            : [];
+    }
+}
+
+class UpcomingCourse {
+    private $conn;
+
+    public function __construct(mysqli $conn) {
+        $this->conn = $conn;
+    }
+
+    /** @return array<int,array> */
+    public function getUpcomingCourses(): array {
+        $sql = "SELECT * FROM Course_tbl WHERE is_upcoming = 1 ORDER BY course_name";
+        $res = $this->conn->query($sql);
+        return $res
+            ? $res->fetch_all(MYSQLI_ASSOC)
+            : [];
+    }
+}
+?>
