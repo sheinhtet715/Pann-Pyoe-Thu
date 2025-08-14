803e6ce393e68e02834bafe63affcfa236c0e272
sheinhtet715 <sheinhtet715@gmail.com>
2025-08-15 00:31:36 +0630

    boostrap admin side fix

--- Admin/layouts/master.php
+++ Admin/layouts/master.php
@@ -43,7 +43,7 @@ if ($user_id) {
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+
     <!-- Custom styles for this template-->
     <link href="../css/sb-admin-2.min.css" rel="stylesheet">
 <style>
