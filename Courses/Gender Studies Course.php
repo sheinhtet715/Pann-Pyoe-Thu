24975b2cdb4b7fa96aaa99be0b52ae9b0b8d55c8
Marina Aye Thidar Tun <marinaayethidartun@gmail.com>
2025-08-21 13:51:05 +0630

    Course Link

--- Courses/Gender Studies Course.php
+++ Courses/Gender Studies Course.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../HomePimg/Logo.ico" type="image/x-icon">
     <title>Gender Studies</title>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="Course.css">
+    <link rel="stylesheet" href="../Courses/Gender.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
--- Courses/ICT Project Management.php
+++ Courses/ICT Project Management.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Collaboration Course</title>
+    <link rel="icon" href="../HomePimg/Logo.ico" type="image/x-icon">
+    <title>ICT Project Management</title>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="Course.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
