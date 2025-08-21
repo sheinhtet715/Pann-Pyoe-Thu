74fcb561c0bda112c84da5a72cb18efe350f9a31
Marina Aye Thidar Tun <marinaayethidartun@gmail.com>
2025-08-21 15:22:07 +0630

    Font

--- Courses/Collaboration Course.php
+++ Courses/Collaboration Course.php
@@ -16,6 +16,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../HomePimg/Logo.ico" type="image/x-icon">
+     <link href="https://fonts.googleapis.com/css?family=Great+Vibes:400,700&display=swap" rel="stylesheet">
     <title> Collaboration Course - PPT</title>
      <link href="https://fonts.googleapis.com/css?family=Great+Vibes:400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet"> 
