f198790570bea117c46ccb3e19aa5bcdd67cd116
sheinhtet715 <sheinhtet715@gmail.com>
2025-08-26 22:15:17 +0630

    course text 320 responsive

--- PHP/Counsellor.php
+++ PHP/Counsellor.php
@@ -147,6 +147,7 @@ ob_start();
 ?>
   <link rel="stylesheet" href="../CSS/Counsellor.css" />
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
   <!-- SweetAlert2 JS -->
   <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>  -->
